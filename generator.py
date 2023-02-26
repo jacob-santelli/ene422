41dc503bbeb1740ed906337832c713f0d9520b27
Ian <im6@princeton.edu>
2023-02-25 20:38:56 -0500

    generator.py added

--- generator.py
+++ generator.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 #--------------------------------------------
 # generator.py
-# Author: Jacob Santelli and Ian Murphy
+# Author: Jacob Santelli and Ian Murray
 #--------------------------------------------
+class Generator:
+  def __init__(self, gentype, capacity, fixom, varom, fuel_cost):
+    self.gentype = gentype
+    self.capacity = capacity
+    self.fixom = fixom
+    self.varom = varom
+    self.fuel_cost = fuel_cost
+
+    # marginal costs of production
+    self.marginal_cost = self.fuel_cost + self.varom
+
+  def getCapacity(self):
+    return self.capacity
